c8e0667bb07f5ec21be33961049c5d9974e3753d
AngelaPan82 <angelapanovska82@gmail.com>
2021-02-13 22:36:46 +0100

    comit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,37 +1,14 @@
-import React, { useState, useEffect } from "react";
-import ReactMarkdown from "react-markdown";
-import axios from "axios";
-import { Container } from "react-bootstrap";
-
-const axParam = {
- 		// Link to Home.md on github repositories
-  baseURL: "https://raw.githubusercontent.com/AngelaPan82/proektna/main/Home.md",
-  headers: {"Content-type": "application/text"}
-}; 
-// dfsdfsdfsfd
-
-const axi = axios.create(axParam);
+import React from "react";
+import '../../App.css'
+import Background from '../elements/Background'
   
-export const Home = () =>{
-	const [ homeTekst, setHomeTekst ] = useState('Read-me.md');
-
-	useEffect(() => {
-
-		axi.get()
-    		.then((resp) => { 
-				setHomeTekst(resp.data);
-    		})
-    	.catch((error) => {
-        	console.warn(error);
-    	});
-
-	},[]);
+function Home(){
 
 	return(
-		<Container style={{marginTop:"1.5em"}}>
-			<div>
-				<ReactMarkdown source={homeTekst} />
-			</div> 
-		</Container>
+		<div>
+			<Background />
+		</div>
 	)
 }
+
+export default Home;
