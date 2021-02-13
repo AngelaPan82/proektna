5dc9ce7ba574e9f6e13152e88e5484a2c3308188
AngelaPan82 <angelapanovska82@gmail.com>
2021-02-13 23:08:00 +0100

    comit

--- src/elements/Background.jsx
+++ src/elements/Background.jsx
@@ -6,11 +6,10 @@ export default function Background() {
     return (
         <div className='background-container'>
             <video src="/videos/video-1.mp4" autoPlay loop muted />
-            <h1>SOMETHING</h1>
-            <p>text</p>
+            <h1>WELCOME</h1>
             <div className="background-btns">
                 <Button className='btns' buttonStyle='btn--outline'
-                    buttonSize='btn--large'>CLICK</Button>
+                    buttonSize='btn--large' to='/apod'>APOD</Button>
 
             </div>
         </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../../App.css'
+import '../App.css'
 import Background from '../elements/Background'
   
 function Home(){
