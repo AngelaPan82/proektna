1abe6e386f13d84830b630f043c7c3f1eec50f9b
AngelaPan82 <angelapanovska82@gmail.com>
2021-02-13 18:58:26 +0100

    comit

--- src/elements/Navbar.jsx
+++ src/elements/Navbar.jsx
@@ -27,7 +27,7 @@ function Navbar() {
          <nav className="navbar">
              <div className="navbar-container">
                  <Link to="/" className="navbar-logo">
-                     APAN <i className="fab fa-typo3"></i>
+                APAN <i class=" fas fa-globe-americas"></i>
                  </Link>
                  <div className='menu-icon' onClick={handleClick}>
                      <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
