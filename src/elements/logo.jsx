b1ec61765f4bcc4f60f163c5cf4563988dea52a6
Angela <77923942+AngelaPan82@users.noreply.github.com>
2021-02-16 11:39:47 +0100

    Add files via upload

---
+++ src/elements/logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../logo192.png'; // Tell webpack this JS file uses this image
+
+const Logo = () => {
+  // Import result is the URL of your image
+  return (
+    <div className="apanLogo">
+      <img src={logo} alt="Logo" />;
+    </div>
+  )
+}
+
+export default Logo;
