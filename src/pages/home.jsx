657d322c30e2a0dc5ec9900cbce20c01eefefeba
Angela <77923942+AngelaPan82@users.noreply.github.com>
2021-04-12 01:44:59 +0200

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Background from '../elements/Background'
 import GitText from "../elements/GitTextLoader";
-  
+
+// ova go prikazuva home ekranot
+// pozadinata i tekstot AboutAs.md
 function Home(){
 
 	return(
