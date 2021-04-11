cb31d489744441537a3cdbd5dfc06c5ce37d05c8
Angela <77923942+AngelaPan82@users.noreply.github.com>
2021-04-12 01:54:59 +0200

    Update logo.jsx

--- src/elements/logo.jsx
+++ src/elements/logo.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from '../logo192.png'; // Tell webpack this JS file uses this image
-
+//
+// logoto na aplikacijata, ne sum sig. dali se koristi DA PROVERAM
+//
 const Logo = () => {
   // Import result is the URL of your image
   return (
