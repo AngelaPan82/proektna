22d75b626c503262bd023dc285f170c544946595
Angela <77923942+AngelaPan82@users.noreply.github.com>
2021-04-12 01:31:25 +0200

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+// GitText komponenta se koristi na poveke mesta
 import GitText from "../elements/GitTextLoader";
-  
+//
+// About stranata, ovde se koristi komponentata za prevzemawe na tekst od git-hub.  
+//
 export const About = () =>{
 
 	return(
