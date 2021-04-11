376d2ca914b59f9a08cec075970e14d541dbb669
Angela <77923942+AngelaPan82@users.noreply.github.com>
2021-04-12 00:23:10 +0200

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,36 +1,12 @@
-import React, { useState, useEffect } from "react";
-import ReactMarkdown from "react-markdown";
-import axios from "axios";
-import { Container, Card } from "react-bootstrap";
-
-const axParam = {
-					// Link to About.md on github repositories 
-  baseURL: "https://raw.githubusercontent.com/AngelaPan82/proektna/master/public/text/AboutUs.md",
-	headers: {
-		"Content-type": "application/text"
-	}
-};
-
-const axi = axios.create(axParam);
+import React from "react";
+import { Container } from "react-bootstrap";
+import GitText from "../elements/GitTextLoader";
   
 export const About = () =>{
-	const [rdm, setRdm] = useState('Loading Abnout.md');
-	
-	useEffect(() => {
-		axi.get()
-    .then((response) => { 
-      setRdm(response.data);
-    })
-    .catch((error) => {
-        console.warn(error);
-    });
-	},[]);
 
 	return(
 		<Container style={{ display: "flex", flexWrap: "wrap",alignContent: "center", maxWidth:"800px" }}>
-			<Card style={{marginTop:"1.5em", padding: "3em"}}>
-				<ReactMarkdown source={rdm} />
-			 </Card>
+				<GitText textFile={"AboutUs.md"} />
 		</Container>
 	)
 }
