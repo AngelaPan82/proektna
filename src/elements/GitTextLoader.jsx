028d372e671d484b062c81563c93556741fff0fd
Angela <77923942+AngelaPan82@users.noreply.github.com>
2021-04-12 01:48:11 +0200

    Update GitTextLoader.jsx

--- src/elements/GitTextLoader.jsx
+++ src/elements/GitTextLoader.jsx
@@ -4,33 +4,35 @@ import axios from "axios";
 import { Card } from "react-bootstrap";
 
 const axParam = {
-					// Link to About.md on github repositories 
+  // Link to About.md on github repositories 
   baseURL: "https://raw.githubusercontent.com/AngelaPan82/proektna/master/public/text/",
-	headers: {
-		"Content-type": "application/text"
-	}
+  headers: {
+	"Content-type": "application/text"
+  }
 };
 
 const axi = axios.create(axParam);
-  
+//
+// Prevzemanje na tekst direktno od GitHub kako props parametar se dobiva textFile od komponentata od kade se povikuva
+// 
 export const GitText = (props) =>{
 	const [rdm, setRdm] = useState('Loading '+ props.textFile);
 	
 	useEffect(() => {
-		axi.get(props.textFile)
-    .then((response) => { 
-      setRdm(response.data);
-    })
-    .catch((error) => {
-        console.warn(error);
-    });
+	axi.get(props.textFile)
+    		.then((response) => { 
+      			setRdm(response.data);
+    		})
+    		.catch((error) => {
+        		console.warn(error);
+   		 });
 	},[props.textFile]);
 
-	return(
-			<Card style={{marginTop:"1.5em", padding: "3em"}}>
-				<ReactMarkdown source={rdm} />
-			</Card>
-	)
+return(
+	<Card style={{marginTop:"1.5em", padding: "3em"}}>
+		<ReactMarkdown source={rdm} />
+	</Card>
+      )
 }
 
 export default GitText;
